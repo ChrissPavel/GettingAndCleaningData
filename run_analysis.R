8b2faffc691a45b0728768b51926fe2513a93a12
ChrissPavel <ChrissPavel@users.noreply.github.com>
2015-01-23 16:55:42 -0500

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,60 @@
+# By: Christian Llano
+
+#setwd("~/UCI HAR Dataset") # Setting workspace directory to folder UCI HAr Dataset 
+
+library("plyr", lib.loc="C:/Program Files/R/R-3.1.2/library")
+feats<-read.table("features.txt") #read features.txt
+actvty<-read.table("activity_labels.txt") #reading activity_labels.txt
+
+#Reading files from train folder
+
+X_subTr<-read.table("./train/subject_train.txt",col.names=c("Subject"))
+X_Tr<-read.table("./train/X_train.txt")
+y_Tr<-read.table("./train/y_train.txt",col.names=c("Activity"))
+
+
+#Reading files from test folder
+
+X_subTst<-read.table("./test/subject_test.txt",col.names=c("Subject"))
+X_Tst<-read.table("./test/X_test.txt")
+y_Tst<-read.table("./test/y_test.txt",col.names=c("Activity"))
+
+#Changing the feature names from second column of feats
+#these are going to be our column names for the required data frame.
+#For this reason i'm going to replace minus and comma by underscore, and remove parenthesis, so names
+#of columns were readable.
+
+feats[,2]<-gsub("-|\\,","_",feats[,2]) #changing minus or comma by underscore
+feats[,2]<-gsub("\\(|\\)","",feats[,2]) #removing parenthesis,here \\ in quotes of first argumet of gsub,for specific strings
+
+
+#Now, binding by rows "features" and X files of train an test 
+
+data<-rbind(unlist(feats[,2]),X_Tr,X_Tst) 
+
+#Next, rename header with first row of "data", where are placed the column names
+colnames(data)<-data[1,] 
+data<-data[-1,] #removing first row of names, we need it no more
+
+#Adding Subject and Activity, ulinst() onlny for converting rbind data.frame to vector
+data$Subject<-unlist(rbind(X_subTr,X_subTst)) #adding Subject column as a new variable 
+data$Activity<-unlist(rbind(y_Tr,y_Tst)) #adding Activity column as a new variable
+
+#Renaming Subject and Activity to its corresponing names
+
+data$Activity <- mapvalues(data$Activity, from = 1:6, 
+                           to = c("WALKING", "WALKING_UPSTAIRS","WALKING_DOWNSTAIRS",
+                                  "SITTING","STANDING","LAYING"))
+
+
+#
+#dim(data)
+#[1] 10299   563
+#We have 10299 rows and 563 coolumns
+
+#Finally, choose the indexes of columns which has mean or standard deviation 
+#Additional Im taking the subject (index 562) and activity (index 563) column
+
+
+ind<-grep("Mean|mean|std",feats[,2])
+data_Result<-data[,c(ind,562,563)]
